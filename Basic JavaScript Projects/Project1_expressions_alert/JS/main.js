00d56a15bec1bc2b6a69711181ef26dabf225440
ThudRogers <josephmleiter@gmail.com>
2025-08-16 19:10:42 -0400

    submitting JS assignment
    pushing basic js assignments.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var Sent1 = "The wizard says to watch out for Goblins";
+var Sent2 = "Their numbers are many and they don't like  wizards"; // Define the sentences to be alerted
+
+
+window.alert(Sent1  + Sent2); // This will show an alert with a message 
+
+
+document.write(3 + 4); // This will output 7    
+document.write(5 * 6); // This will output 30
+document.write(10 - 2); // This will output 8
+document.write(20 / 4); // This will output 5
