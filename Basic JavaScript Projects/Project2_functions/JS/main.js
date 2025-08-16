6877b0818b92097eaa2fd8aff2aea72e4be7e6f6
ThudRogers <josephmleiter@gmail.com>
2025-08-16 19:27:21 -0400

    pushing more work
    pushing some JS work to the github

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var sentence = "The wizard says to watch out for Goblins";
+    sentence += " Their numbers are many and they don't like wizards"; // Concatenate the sentences
+    document.getElementById("Concatenate").innerHTML = sentence; // Display the concatenated text in the HTML element
+}
+
+function warningFunction() {
+    var warning = "You went the wrong way!"; // Define the warning message
+    document.getElementById("Warning").innerHTML = warning; // Display the warning message in the HTML element
+}
